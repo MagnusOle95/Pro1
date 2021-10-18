8ffdad2a04398782c7cfbee8b9e103da5c03ffaa
magnu <magnus.s.larsen@hotmail.com>
2021-10-18 16:15:28 +0200

    Dag 16 Uploade.

---
+++ Dag_16_Test/src/Opgave1/Person.java
@@ -0,0 +1,27 @@
+package Opgave1;
+
+public class Person {
+	private String firstName;
+	private String title;
+
+
+	public Person(String firstName, String title) {
+		this.firstName = firstName;
+		this.title = title;
+
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	@Override
+	public String toString() {
+		return title + " " + firstName;
+	}
+
+}
