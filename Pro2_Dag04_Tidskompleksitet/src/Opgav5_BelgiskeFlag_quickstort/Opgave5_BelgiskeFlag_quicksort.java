0cf1a285aab673ad1709e66f628bac5e6bd26176
magnu <magnus.s.larsen@hotmail.com>
2022-02-09 15:03:21 +0100

    quick sort metode.

--- Pro2_Dag04_Tidskompleksitet/src/Opgav5_BelgiskeFlag_quickstort/Opgave5_BelgiskeFlag_quicksort.java
+++ Pro2_Dag04_Tidskompleksitet/src/Opgav5_BelgiskeFlag_quickstort/Opgave5_BelgiskeFlag_quicksort.java
@@ -7,18 +7,56 @@ public class Opgave5_BelgiskeFlag_quicksort {
     public static void main(String[] args) {
         char[] flag = new char[]{ 'G','R','S','G','G','R','S','R','G','G','R','S','G'};
         System.out.println(flag);
+        belgiskFlag(flag);
+        System.out.println(flag);
     }
 
 
 
-    public void belgiskFlag(char[] belgisk_flag){
-        quicksortFlag(belgisk_flag,0 ,belgisk_flag.length - 1);
+    public static void belgiskFlag(char[] belgisk_flag){
+        quicksortFlagHighToLow(belgisk_flag,0 ,belgisk_flag.length - 1);
+
     }
 
-    private void quicksortFlag(char[] flag, int low, int high){
+    private static void quicksortFlagHighToLow(char[] flag, int low, int high){
         if (low < high){
+            int s = sortPartitionHighToLow(flag,low ,high);
+            quicksortFlagHighToLow(flag,low ,s - 1);
+            quicksortFlagHighToLow(flag,s +1,high);
 
         }
+    }
+
+
+    private static int sortPartitionHighToLow(char[] flag, int low, int high){
+        int e = flag[low];
+        int i = low + 1;
+        int j = high;
+
+        while (i <= j){
+            if (flag[i] >= e){
+                i++;
+            }
+            else if (flag[j] < e){
+                j--;
+            }
+            else{
+                swap(flag,i,j);
+                i++;
+                j--;
+            }
+        }
+        swap(flag,low ,j);
+
+        return j;
+    }
+
+
+
+    private static void swap(char[] flag, int i, int j) {
+        char temp = flag[i];
+        flag[i] = flag[j];
+        flag[j] = temp;
     }
 
 
