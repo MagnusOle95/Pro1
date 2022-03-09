11013b783d1c26104348932fa075984657392212
Oleand <magnus.s.larsen@hotmail.com>
2022-03-09 21:34:58 +0100

    Opgave 4 wtf mangler at spørge imorgen-

---
+++ Pro2_Dag12_Designpattern_singleton_Adapter/src/Opgaev5_Dag9/App.java
@@ -0,0 +1,12 @@
+package Opgaev5_Dag9;
+
+public class App {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+}
---
+++ Pro2_Dag12_Designpattern_singleton_Adapter/src/Opgaev5_Dag9/Test_af_bag.java
@@ -0,0 +1,7 @@
+package Opgaev5_Dag9;
+
+public class Test_af_bag {
+
+
+
+}
