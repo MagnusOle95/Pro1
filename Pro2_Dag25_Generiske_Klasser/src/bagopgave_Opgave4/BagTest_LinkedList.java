2e627b6f22a68398c3eb6271b5ab4b4f4f0b4fa2
Oleand <magnus.s.larsen@hotmail.com>
2022-05-16 22:59:10 +0200

    Magter ikke mere.

--- Pro2_Dag25_Generiske_Klasser/src/bagopgave_Opgave4/BagTest_LinkedList.java
+++ Pro2_Dag25_Generiske_Klasser/src/bagopgave_Opgave4/BagTest_LinkedList.java
@@ -1,20 +1,18 @@
-package bagopgave;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+package bagopgave_Opgave4;
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class BagTest
+import static org.junit.Assert.*;
+
+public class BagTest_LinkedList
 {
     private Bag<String> bag;
 
     @Before
     public void setUp()
     {
-        bag = new ArrayBag<String>();
+        bag = new LinkedListBag<>();
         bag.add("Jan");
         bag.add("Per");
         bag.add("Hans");
